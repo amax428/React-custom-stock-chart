d7f42328b3551a7e28dcec65fedcc7c0957afd3a
aaron.goulden <openheart.three@gmail.com>
2019-01-04 18:18:02 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ChartComponent from './component/chartComponent';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ChartComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
